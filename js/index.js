d3785e39cbcc4a9434775d90280df0632878f9e2
SIlviaTivi <skfl.erg@hotmail.com>
2018-01-29 21:14:54 -0500

    hover index js

--- js/index.js
+++ js/index.js
@@ -1,22 +1,37 @@
-/* se ejecuta la función cardify*/
-/*elaborando el plugin-libreria*/
-/*función con clausura*/
 (function($) {
   /*maneja la interaaciones con elementos visuales*/
     $.fn.cardify = function() {
       /*busca todas la imagenes en el contenedor*/
-      $(this).find('img').each(function() {
+      let imagen = $(this).find('img');
+      imagen.each(function() {
         /*a la etiqueta img lo contiene la etiqueta figure*/
-        var figure = $(this).wrap('<figure></figure>');
+        let figure = $(this).wrap('<figure></figure>');
         /* con this se seleciona las imagnes, etiqueta img */
         console.log($(this));
         /*busca el elemento padre de la etiqueta img, es decir figure y le adiciona la etiqueta figcaption*/
         $(this).parent().append($('<figcaption></figcaption>'));
-        /*incorporando el atributo alt a la etiqueta figcaption*/
+        /* incluyendo el hover*/
+      });
+      
+      imagen.on('mouseover', function() {
         /*seleccionando el atributo alt de la etiqueta img*/
-        var textImagen = $(this).attr('alt')
+        let textImagen = $(this).attr('alt')
         /*seleccionando el elemento que le precede a la etiqueta img,que es figcaption*/
-        $(this).next().text(textImagen);
+        let text = $(this).next().text(textImagen);
+        $(this).css({'opacity': '0.1',});
+        text.css({'display': 'inline'});
+      });
+
+      imagen.on('mouseout', function(text) {
+             /*seleccionando el atributo alt de la etiqueta img*/
+        let textImagen2 = $(this).attr('alt')
+        /*seleccionando el elemento que le precede a la etiqueta img,que es figcaption*/
+        let text2 = $(this).next().text(textImagen2);
+        $(this).css({'opacity': '1',});
+        text2.css({'display': 'none'});
       });
     }
   })(jQuery);
+
+
+ 
