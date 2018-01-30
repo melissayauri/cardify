aab379a341403d37038d283d27d739b749544b13
melissayauri <yaurimelissa@gmail.com>
2018-01-30 11:58:17 -0500

    mejorando las variables

--- js/index.js
+++ js/index.js
@@ -5,18 +5,31 @@
   /*maneja la interaaciones con elementos visuales*/
     $.fn.cardify = function() {
       /*busca todas la imagenes en el contenedor*/
-      $(this).find('img').each(function() {
+      let imagen = $(this).find('img');
+      imagen.each(function() {
         /*a la etiqueta img lo contiene la etiqueta figure*/
-        var figure = $(this).wrap('<figure></figure>');
+        let figure = $(this).wrap('<figure></figure>');
         /* con this se seleciona las imagnes, etiqueta img */
         console.log($(this));
         /*busca el elemento padre de la etiqueta img, es decir figure y le adiciona la etiqueta figcaption*/
         $(this).parent().append($('<figcaption></figcaption>'));
         /*incorporando el atributo alt a la etiqueta figcaption*/
         /*seleccionando el atributo alt de la etiqueta img*/
-        var textImagen = $(this).attr('alt')
+      /*  let textImagen = $(this).attr('alt')*/
         /*seleccionando el elemento que le precede a la etiqueta img,que es figcaption*/
-        $(this).next().text(textImagen);
+        /*let text = $(this).next().text(textImagen);*/
+        /*text.css({'display': 'none'
+      });*/
+        /* incluyendo el hover*/
       });
+      /*
+      imagen.on('mouseover', function() {
+      $(this).css({
+        'opacity': '0.1',
+      });
+    });
+    imagen.on('mouseout', function() {
+
+    });*/
     }
   })(jQuery);
