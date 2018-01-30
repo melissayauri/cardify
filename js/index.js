4dfa005805fdcd4a086ecaa57ec7c0d9d62405e4
SIlviaTivi <skfl.erg@hotmail.com>
2018-01-30 00:13:24 -0500

    añadiendo hover a index.js

--- js/index.js
+++ js/index.js
@@ -3,35 +3,37 @@
     $.fn.cardify = function() {
       /*busca todas la imagenes en el contenedor*/
       let imagen = $(this).find('img');
+
       imagen.each(function() {
-        /*a la etiqueta img lo contiene la etiqueta figure*/
-        let figure = $(this).wrap('<figure></figure>');
-        /* con this se seleciona las imagnes, etiqueta img */
-        console.log($(this));
-        /*busca el elemento padre de la etiqueta img, es decir figure y le adiciona la etiqueta figcaption*/
+         /*a la etiqueta img lo contiene la etiqueta figure*/
+        $(this).wrap('<figure></figure>');
+          /*busca el elemento padre de la etiqueta img, es decir figure y le adiciona la etiqueta figcaption*/
         $(this).parent().append($('<figcaption></figcaption>'));
-        /* incluyendo el hover*/
-      });
-      
-      imagen.on('mouseover', function() {
-        /*seleccionando el atributo alt de la etiqueta img*/
-        let textImagen = $(this).attr('alt')
-        /*seleccionando el elemento que le precede a la etiqueta img,que es figcaption*/
-        let text = $(this).next().text(textImagen);
-        $(this).css({'opacity': '0.1',});
-        text.css({'display': 'inline'});
+
       });
 
-      imagen.on('mouseout', function(text) {
-             /*seleccionando el atributo alt de la etiqueta img*/
-        let textImagen2 = $(this).attr('alt')
+      imagen.mouseover(function() {
+         /*seleccionando el atributo alt de la etiqueta img*/
+        let getAttrOver = $(this).attr('alt');
         /*seleccionando el elemento que le precede a la etiqueta img,que es figcaption*/
-        let text2 = $(this).next().text(textImagen2);
+        let showText = $(this).next().text(getAttrOver);
+        /*mostrando texto de figcaprion*/
+        showText.css({'display': 'inline'});
+        /*opacando imagen*/
+        $(this).css({'opacity': '0.1',});
+
+      });
+      
+      imagen.mouseout(function() {
+         /*seleccionando el atributo alt de la etiqueta img*/
+        let getAttrOut = $(this).attr('alt')
+        /*seleccionando el elemento que le precede a la etiqueta img,que es figcaption*/
+        let outText = $(this).next().text(getAttrOut);
+        /*ocultando texto de figcaprion*/
+        outText.css({'display': 'none'});
+       /*visualizando imagen*/
         $(this).css({'opacity': '1',});
-        text2.css({'display': 'none'});
+
       });
     }
   })(jQuery);
-
-
- 
